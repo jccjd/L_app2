57f08f024f520d03962d01da8b27fe61e02f3b25
jccjd <1340944538@qq.com>
2018-06-20 13:37:08 +0800

    updata

--- app/src/main/java/com/example/lenovo/l_app2/net/RetrofitUtil.java
+++ app/src/main/java/com/example/lenovo/l_app2/net/RetrofitUtil.java
@@ -2,6 +2,8 @@ package com.example.lenovo.l_app2.net;
 
 import com.example.lenovo.l_app2.bean.News;
 import com.example.lenovo.l_app2.bean.Video;
+import com.example.lenovo.l_app2.util.Const;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Retrofit;
@@ -12,7 +14,7 @@ import java.util.List;
 public class RetrofitUtil {
     public static void getNews(Callback<HttpResult<List<News>>> callback) {
         //填写下列代码
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://10.0.2.2")
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(Const.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         ApiService apiService = retrofit.create(ApiService.class);
@@ -21,7 +23,7 @@ public class RetrofitUtil {
     }
 
     public static void getVideo(Callback<HttpResult<List<Video>>> callback) {
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://10.0.2.2").addConverterFactory(GsonConverterFactory.create())
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(Const.BASE_URL).addConverterFactory(GsonConverterFactory.create())
                 .build();
         ApiService apiService = retrofit.create(ApiService.class);
         Call<HttpResult<List<Video>>> call = apiService.getVideo();
